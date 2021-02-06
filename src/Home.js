42e6819f6d7d2e689243f06d00c9da6c74ef52d2
Muhammad Mujahid Riyanto <muhammad.mujahid@ai.astra.co.id>
2021-02-06 12:37:19 +0700

    Set Error Handling
    Set Error Handling

--- src/Home.js
+++ src/Home.js
@@ -2,26 +2,35 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([]);
-
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
-  //   setBlogs(newBlogs);
-  // };
+  const [blogs, setBlogs] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     fetch('http://localhost:8000/blogs')
       .then((res) => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data');
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setLoading(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} />
+      {error && <div>{error}</div>}
+      {loading && <p>Loading...</p>}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
   );
 };
