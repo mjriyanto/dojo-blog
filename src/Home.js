bedab8c05f6c383cb5a19dc5db8fc6cf693aced7
Muhammad Mujahid Riyanto <muhammad.mujahid@ai.astra.co.id>
2021-02-06 10:49:12 +0700

    Set local json server
    Set local json server

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState([]);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // };
+
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} handleDelete={handleDelete} />
+      <BlogList blogs={blogs} />
     </div>
   );
 };
