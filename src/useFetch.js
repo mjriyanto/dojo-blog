e5a40ca15198a351cd76d058aad19ec51070fc0c
Muhammad Mujahid Riyanto <muhammad.mujahid@ai.astra.co.id>
2021-02-10 23:58:57 +0700

    Blog Detail, React Router, Router Parameter, Fetch error handling

--- src/useFetch.js
+++ src/useFetch.js
@@ -4,26 +4,31 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+
   useEffect(() => {
-    setTimeout(() => {
-      setLoading(true);
-      fetch(url)
-        .then((res) => {
-          if (!res.ok) {
-            throw Error('Could not fetch the data');
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setData(data);
-          setLoading(false);
-          setError(null);
-        })
-        .catch((err) => {
+    const abortCont = new AbortController();
+    setLoading(true);
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error('Could not fetch the data');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted');
+        } else {
           setLoading(false);
           setError(err.message);
-        });
-    }, 1000);
+        }
+      });
+    return () => abortCont.abort();
   }, [url]);
 
   return { data, loading, error };
