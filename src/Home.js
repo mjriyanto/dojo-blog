f92b1aa97abe48733430a2e4fe69865c8e184e22
Muhammad Mujahid Riyanto <muhammad.mujahid@ai.astra.co.id>
2021-02-10 08:45:55 +0700

    Creating custom Hooks
    useFetch custom Hooks

--- src/Home.js
+++ src/Home.js
@@ -1,31 +1,10 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch('http://localhost:8000/blogs')
-      .then((res) => {
-        if (!res.ok) {
-          throw Error('Could not fetch the data');
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setLoading(false);
-        setError(null);
-      })
-      .catch((err) => {
-        setLoading(false);
-        setError(err.message);
-      });
-  }, []);
-
+  const url = 'http://localhost:8000/blogs'
+  const { data: blogs, loading, error } = useFetch(url);
   return (
     <div className="home">
       {error && <div>{error}</div>}
