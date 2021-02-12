5d692a29da6b6a2bd0d1eb34f527b5413af516c1
Muhammad Mujahid Riyanto <muhammad.mujahid@ai.astra.co.id>
2021-02-12 21:51:04 +0700

    Create form component including set state

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,44 @@
+import { useState } from 'react';
+
 const Create = () => {
-  return ( 
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const blog = { title, body, author };
+
+    console.log(e);
+    console.log(blog);
+  }
+
+  return (
     <div className="create">
       <h2>Add a new Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <label>Blog body:</label>
+        <textarea
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          required
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add blog</button>
+      </form>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Create;
